911177e1002b975d1cc50d974836ae497e07b516
Evan Fujita <evanfujita@gmail.com>
2020-11-06 03:18:01 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,8 +2,10 @@ def count_strings(array)
   array.count do |element|
     element.class == String
 end
+end
 
 def count_empty_strings(array)
   array.count do |element|
     element == nil
+end
 end
